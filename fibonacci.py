f69e02f379a4aaaedc7ff0c1b8437b9af3921d19
unknown <ph8@UOS-203141.soton.ac.uk>
2016-02-22 11:13:59 +0000

    update fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,16 @@
 def Fibonacci(n):
-    """
-    Return the n-th value of the Fibonacci sequence
-    [0, 1, 1, 2, 3, 5, 8, 13, ...]
-    """
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return Fibonacci(n-1) + Fibonacci(n-2)
-		
+	"""
+	Return the n-th value of the Fibonacci sequence
+	[0, 1, 1, 2, 3, 5, 8, 13, ...]
+	"""
+	if n < 0:
+		raise ValueError("n must be greater than zero")
+	elif round(n) != n:
+		raise ValueError("n must be a whole number")
+	elif n == 0:
+		return 0
+	elif n == 1:
+		return 1
+	else:
+		return Fibonacci(n-1) + Fibonacci(n-2)
+	
