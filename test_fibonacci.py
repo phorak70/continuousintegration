11f7361ae51a57aabb79b02e9dae457c39d5b504
unknown <ph8@UOS-203141.soton.ac.uk>
2016-02-22 11:17:59 +0000

    changed test_fibonacci, (self) added

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -1,7 +1,7 @@
 import unittest
 
 class FibExceptions(unittest.TestCase):
-	def test_fibonacci():
+	def test_fibonacci(self):
 		import fibonacci as fb
 		assert fb.Fibonacci(10) == 55
 		assert fb.Fibonacci(5) == 5
