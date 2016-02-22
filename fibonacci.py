2de8350987ffe5b576dea1725e9aa62743fdfaf3
unknown <ph8@UOS-203141.soton.ac.uk>
2016-02-22 10:40:16 +0000

    Added fibonacci function in file fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,12 @@
+def Fibonacci(n):
+    """
+    Return the n-th value of the Fibonacci sequence
+    [0, 1, 1, 2, 3, 5, 8, 13, ...]
+    """
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return Fibonacci(n-1) + Fibonacci(n-2)
+		
